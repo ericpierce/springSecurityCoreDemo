8029fe9ff9fe4cc851b4dfec0c53012163ef804b
Eric Pierce <eric.d.pierce@gmail.com>
2014-03-18 17:18:12 -0500

    Implement spring security plugin

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
+import com.springSecurityCoreDemo.auth.*
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def adminRole = Role.findOrSaveWhere(authority: 'ROLE_ADMIN')
+        def user = User.findOrSaveWhere(username: 'somebody@gmail.com', password: 'password')
+
+        if (!user.authorities.contains(adminRole)) {
+            UserRole.create(user, adminRole, true)
+        }
     }
+
     def destroy = {
     }
 }
